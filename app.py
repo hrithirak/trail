5b340cf8e823c5d4cede750c28736f2e5b74ca20
Chindalur Rakesh <hchind741@apac.comcast.com>
2025-01-24 12:09:13 +0530

    webhook curl

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask, request
+from flask import Flask, request,jsonify 
+
+import hashlib
+import json
+
+
 
 app = Flask(__name__)
-
-@app.route('/github/webhook', methods=['POST'])
+APP_KEY = b"webhookhrithi"
+@app.route('/webhook', methods=['POST'])
 def handle_webhook():
-    
-    return 'Webhook received successfully Sample!'
+    github_hook_id = request.headers.get('X-Github-Hook-ID')
+    if not github_hook_id:
+        return jsonify({'error':'Missing github ID'}),400
+    try:
+        payload = json.loads(request.get_data())
+    except json.JSONDecodeError:
+        return jsonify({'error': 'Invalid JSON payload'}), 400
+    print('Webhook received successfully Sample!')
+    return 'Webhook'
 
 if __name__ == '__main__':
     app.run(debug=True)
