565a2b29832171676e19fa1b8e7a73310e44a7cf
Chindalur Rakesh <hchind741@apac.comcast.com>
2025-01-27 14:52:46 +0530

    trail 3

--- app.py
+++ app.py
@@ -1,27 +1,34 @@
 from flask import Flask, request,jsonify 
 import json
-
-
-
+import os 
+os.environ['APP_ID'] =  '1119711'
+os.environ['APP_WEBHOOK_SECERT'] = "hrithiwebhook"
+os.environ['APP_CLIENT_ID'] = "Iv23li6xO8obaZANu92V"
 app = Flask(__name__)
-APP_KEY = b"webhookhrithi"
+
 @app.route('/webhook', methods=['POST'])
 def handle_webhook():
-    github_webhooks_value = request.args.get('Github-webhooks')
-    if github_webhooks_value:
-        print(github_webhooks_value)
-    else:
-        return "Github-webhooks parameter not found", 400
-    github_hook_id = request.headers.get('X-Github-Hook-ID')
-    print(github_hook_id)
-    if not github_hook_id:
-        return jsonify({'error':'Missing github ID'}),400
+   # github_webhooks_value = request.args.get('Github-webhooks')
+    #if github_webhooks_value:
+     #   print(github_webhooks_value)
+    #else:
+     #   return "Github-webhooks parameter not found", 400
+    event = request.headers.get('X-Github-Event')
+    # if not event:
+    #     return "event not found"
+    # else:
+    #     return "event found"
+    # github_hook_id = request.headers.get('X-Github-Hook-ID')
+    # print(github_hook_id)
+    # if not github_hook_id:
+    #     return jsonify({'error':'Missing github ID'}),400
     try:
         payload = json.loads(request.get_data())
+        print(payload)
     except json.JSONDecodeError:
         return jsonify({'error': 'Invalid JSON payload'}), 400
     print('Webhook received successfully Sample!')
-    return 'Webhook'
+    return 
 
 if __name__ == '__main__':
     app.run(debug=True)
